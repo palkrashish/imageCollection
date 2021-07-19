78fc588a74f9ec6443263899aac21d84dc8bbd41
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2021-07-19 19:16:01 +0530

    Add files via upload
    adding my image

---
+++ skillsData.js
@@ -0,0 +1,35 @@
+export const skillList = [
+    {
+        skillName: 'C++',
+        skillUrl: 'https://drive.google.com/file/d/1KiT7rFjfhPkkUlbp4YRJ5tHTCq3VjPdW/view?usp=sharing',
+    },
+    {
+        skillName: 'HTML',
+        skillUrl: 'https://madhavbahl.tech/portfolio-assets/html.png',
+    },
+    {
+        skillName: 'CSS',
+        skillUrl: 'https://madhavbahl.tech/portfolio-assets/css.png',
+    },
+    {
+        skillName: 'JavaScript',
+        skillUrl: 'https://madhavbahl.tech/portfolio-assets/js.png',
+    },
+    {
+        skillName: 'React',
+        skillUrl: 'https://madhavbahl.tech/portfolio-assets/react.png',
+    },
+    {
+        skillName: 'Redux',
+        skillUrl: 'https://madhavbahl.tech/portfolio-assets/redux.png',
+    },
+    {
+        skillName: 'NodeJS',
+        skillUrl: 'https://madhavbahl.tech/portfolio-assets/node.png',
+    },
+    {
+        skillName: 'TypeScript',
+        skillUrl: 'https://madhavbahl.tech/portfolio-assets/ts.png',
+    },
+   
+];
